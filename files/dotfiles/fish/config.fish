c1b1e2662e1de243d4de9bcb130d62a7219a368d
Grzegorz Dlugoszewski <g.dlugoszewski@gmail.com>
2019-12-29 15:45:57 +0100

    Add config.fish and fisher package manager

---
+++ files/dotfiles/fish/config.fish
@@ -0,0 +1,19 @@
+# set -gx PATH ~/workspace/go/bin $PATH
+# set -gx PATH /snap/bin/ $PATH
+# set -gx PATH ~/.npm-global/bin $PATH
+# set -gx PATH ~/.pyenv/bin $PATH
+# set -gx PATH ~/bin $PATH
+
+#Disable fish greeting
+set fish_greeting
+
+alias lll "ls -la"
+alias kc "kubectl"
+alias kx "kubectx"
+alias kn "kubens"
+alias kt "kubetail"
+alias mgs "mgitstatus ~/workspace 3"
+
+# Pyenv support
+# status --is-interactive; and pyenv init - | source
+# status --is-interactive; and pyenv virtualenv-init - | source

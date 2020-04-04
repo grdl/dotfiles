3141cab5c17b2e8132798fdc09b52b5cf652b4ca
Grzegorz Dlugoszewski <g.dlugoszewski@gmail.com>
2020-04-04 22:27:44 +0200

    Replace pure with spaceship prompt
    Cleanup config.fish:
    - Remove old commended lines
    - Better way to set $PATH variable
    (https://github.com/fish-shell/fish-shell/issues/527#issuecomment-426565588)

--- files/dotfiles/fish/config.fish
+++ files/dotfiles/fish/config.fish
@@ -1,15 +1,11 @@
-# set -gx PATH ~/workspace/go/bin $PATH
-# set -gx PATH /snap/bin/ $PATH
-# set -gx PATH ~/.npm-global/bin $PATH
-# set -gx PATH ~/.pyenv/bin $PATH
-
 # we put downloaded binaries to ~/.local/bin, make sure it's added to $PATH
-set -gx PATH ~/.local/bin $PATH
+set fish_user_paths $PATH ~/.local/bin 
+
 
 # set flux namespace
 set -gx FLUX_FORWARD_NAMESPACE flux
 
-#Disable fish greeting
+# Disable fish greeting
 set fish_greeting
 
 alias lll "ls -la"
@@ -19,6 +15,6 @@ alias kn "kubens"
 alias kt "kubetail"
 alias mgs "mgitstatus --depth 3 ~/workspace"
 
-# Pyenv support
-# status --is-interactive; and pyenv init - | source
-# status --is-interactive; and pyenv virtualenv-init - | source
+# Enable starship prompt
+starship init fish | source
+

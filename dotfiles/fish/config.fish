83d699143a727be92b4964c13572f53ab0b53b9e
Grzegorz Dlugoszewski <g.dlugoszewski@gmail.com>
2020-04-06 20:57:43 +0200

    Add Brewfile and update shell configs
    - Update Alacritty paths to point to fish and tmux installed via
      Homebrew
    - Remove default shell from tmux config, it should be enough to start
      tmux from the shell you need
    - Add Homebrew envs to config.fish

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -2,18 +2,30 @@
 set fish_user_paths $PATH ~/.local/bin 
 
 
+# Add Homebrew to PATH
+set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
+set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
+set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
+set -g fish_user_paths "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $fish_user_paths;
+set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH;
+set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH;
+
+
 # set flux namespace
 set -gx FLUX_FORWARD_NAMESPACE flux
+
 
 # Disable fish greeting
 set fish_greeting
 
+# Set aliases
 alias lll "ls -la"
 alias kc "kubectl"
 alias kx "kubectx"
 alias kn "kubens"
 alias kt "kubetail"
 alias mgs "mgitstatus --depth 3 ~/workspace"
+
 
 # Enable starship prompt
 starship init fish | source

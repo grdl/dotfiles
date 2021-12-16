e6390959feaa18b730d0528c8eb1efe87faa1da7
Grzegorz Dlugoszewski <g.dlugoszewski@gmail.com>
2021-12-16 12:54:59 +0100

    Add formatters to lvim

--- dotfiles/vim/lvim-config.lua
+++ dotfiles/vim/lvim-config.lua
@@ -5,13 +5,13 @@ lvim.builtin.terminal.active = true
 
 -- Enable inline git blame
 lvim.builtin.gitsigns.opts = {
-  -- Disable gitsigns keymapping. They conflict with <leader>+h and are added to which_key anyway.
-  keymaps = {},
-  current_line_blame = true,
-  current_line_blame_opts = {
-    delay = 0,
-    virt_text_pos = "right_align",
-  },
+	-- Disable gitsigns keymapping. They conflict with <leader>+h and are added to which_key anyway.
+	keymaps = {},
+	current_line_blame = true,
+	current_line_blame_opts = {
+		delay = 0,
+		virt_text_pos = "right_align",
+	},
 }
 
 -- Use icons instead of highlights for git status in nvim_tree
@@ -29,35 +29,34 @@ lvim.builtin.treesitter.ensure_installed = "maintained"
 lvim.builtin.which_key.mappings["gL"] = { "<cmd>Gitsigns toggle_current_line_blame<CR>", "Line Blame Toggle" }
 lvim.builtin.which_key.mappings["t"] = { "<cmd>TroubleToggle<CR>", "Trouble" }
 
-
 lvim.builtin.which_key.mappings["s"] = {
-    name = "Search",
-    e = { "<cmd>Telescope file_browser<cr>", "File Browser" },
-    f = { "<cmd>Telescope find_files<cr>", "Files" },
-    H = { "<cmd>Telescope help_tags<cr>", "Help Tags" },
-    h = { "<cmd>Telescope search_history<cr>", "Search history" },
-    M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
-    p = { "<cmd>Telescope projects<CR>", "Projects" },
-    b = { "<cmd>Telescope buffers<cr>", "Buffers" },
-    B = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Current Buffer" },
-    g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
-    R = { "<cmd>Telescope resume<cr>", "Resume" },
-    k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
-    C = { "<cmd>Telescope commands<cr>", "Commands" },
-    c = { "<cmd>lua require('telescope.builtin.internal').colorscheme({enable_preview = true})<cr>", "Colorscheme" },
+	name = "Search",
+	e = { "<cmd>Telescope file_browser<cr>", "File Browser" },
+	f = { "<cmd>Telescope find_files<cr>", "Files" },
+	H = { "<cmd>Telescope help_tags<cr>", "Help Tags" },
+	h = { "<cmd>Telescope search_history<cr>", "Search history" },
+	M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
+	p = { "<cmd>Telescope projects<CR>", "Projects" },
+	b = { "<cmd>Telescope buffers<cr>", "Buffers" },
+	B = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Current Buffer" },
+	g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
+	R = { "<cmd>Telescope resume<cr>", "Resume" },
+	k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+	C = { "<cmd>Telescope commands<cr>", "Commands" },
+	c = { "<cmd>lua require('telescope.builtin.internal').colorscheme({enable_preview = true})<cr>", "Colorscheme" },
 }
 
 -- Additional Plugins
 lvim.plugins = {
-    {
-      "folke/trouble.nvim",
-      cmd = "TroubleToggle",
-    },
+	{
+		"folke/trouble.nvim",
+		cmd = "TroubleToggle",
+	},
 }
 
 lvim.autocommands.custom_groups = {
-  -- Refresh NvimTree after lazygit terminal is closed
-  { "TermLeave", "*", "NvimTreeRefresh" },
+	-- Refresh NvimTree after lazygit terminal is closed
+	{ "TermLeave", "*", "NvimTreeRefresh" },
 }
 
 vim.o.relativenumber = true
@@ -67,9 +66,22 @@ vim.o.cmdheight = 1
 lvim.builtin.telescope.defaults.layout_config.width = 0.9
 
 -- Remove the stupid scrollbar from lualine
-lvim.builtin.lualine.sections.lualine_x = {"encoding", "filetype"}
-lvim.builtin.lualine.sections.lualine_y = { "location", "progress"}
+lvim.builtin.lualine.sections.lualine_x = { "encoding", "filetype" }
+lvim.builtin.lualine.sections.lualine_y = { "location", "progress" }
 lvim.builtin.lualine.sections.lualine_z = {}
 
 -- Don't show inline diagnostics messages
 lvim.lsp.diagnostics.virtual_text = false
+lvim.lsp.automatic_servers_installation = true
+
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{
+		exe = "prettier",
+	},
+	{
+		exe = "stylua",
+	},
+})
+
+lvim.format_on_save = true

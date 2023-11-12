c9617836532bc49e5b49d3ab7f80f992bc1fbfa5
Grzegorz Dlugoszewski <g.dlugoszewski@gmail.com>
2023-11-12 10:23:13 +0100

    Clean up config.fish, remove loadenv and encrypted env file

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,21 +1,18 @@
-# Add ~/.local/bin to PATH
+# Set Vim as default editor
+set -gx EDITOR nvim
+
+# Add stuff to PATH
 set PATH ~/.local/bin $PATH
-
-# Add Rust binaries to PATH
 set PATH ~/.cargo/bin $PATH
-
-# Add Go binaries to PATH
 set PATH ~/go/bin $PATH
+set PATH ~/.krew/bin $PATH
+set PATH ~/.poetry/bin $PATH
 
 # Set up Pyenv
 set -x PYENV_ROOT $HOME/.pyenv
 set PATH $PYENV_ROOT/bin $PATH
 pyenv init - | source
 status is-interactive; and pyenv init --path | source
-
-set PATH $HOME/.poetry/bin $PATH
-
-set -gx EDITOR lvim
 
 # Set XDG_CONFIG_HOME to load k9s config
 set -gx XDG_CONFIG_HOME $HOME/.config
@@ -26,25 +23,20 @@ set fish_greeting
 # Set aliases
 alias ll "lsd -hAl --group-dirs first --date relative --total-size --icon never"
 alias lll "lsd -hAl --group-dirs first --date date --icon never"
-alias kx "kubectx"
-alias kn "kubens"
-alias kt "kubetail"
-alias vim "lvim"
 
+abbr --erase (abbr --list)
 abbr -g kc kubectl
+abbr -g kx kubectx
+abbr -g kn kubens
+abbr -g kt kubetail
 abbr -g tf terraform
 abbr -g gs git status
 abbr -g gg lazygit
 abbr -g hh history merge
 abbr -g b br
 abbr -g bb br -hdps
-abbr -g vv vim .
-abbr -g av aws-vault exec 
-
+abbr -g vv nvim .
+abbr -g av aws-vault exec
 
 # Enable starship prompt
 starship init fish | source
-
-
-# Load env variables from envrironment file (requires sops)
-loadenv $__fish_config_dir/environment

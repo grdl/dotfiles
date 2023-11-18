264e71cb8435530026d59d84a9f83e027ec6615c
Grzegorz Dlugoszewski <g.dlugoszewski@gmail.com>
2023-11-18 09:43:31 +0100

    Add gitlinker plugin and "open in browser" mapping

--- dotfiles/nvim/lua/plugins/plugins.lua
+++ dotfiles/nvim/lua/plugins/plugins.lua
@@ -7,4 +7,20 @@ return {
       colorscheme = "nord",
     },
   },
+
+  {
+    "ruifm/gitlinker.nvim",
+    config = function()
+      require("gitlinker").setup({
+        opts = {
+          action_callback = require("gitlinker.actions").open_in_browser,
+          print_url = false,
+        },
+        mappings = "<leader>gO",
+      })
+    end,
+    keys = {
+      { "<leader>gO", desc = "Open in browser" },
+    },
+  },
 }

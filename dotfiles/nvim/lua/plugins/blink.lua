d555e8a222642ac332f90d2bd3645323d2b99498
Grzegorz Dlugoszewski <g.dlugoszewski@gmail.com>
2025-08-05 22:38:12 +0200

    Add blink autocompletion plugin

---
+++ dotfiles/nvim/lua/plugins/blink.lua
@@ -0,0 +1,16 @@
+return {
+	"saghen/blink.cmp",
+	version = "1.6.0",
+
+	opts = {
+		keymap = {
+			preset = "enter",
+		},
+
+		completion = {
+			documentation = {
+				auto_show = true,
+			},
+		},
+	},
+}

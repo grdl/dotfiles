e48024570c829546c362faa54b6e81db53e44ea9
Grzegorz Dlugoszewski <g.dlugoszewski@gmail.com>
2025-08-06 23:50:40 +0200

    Add border to documentation popup in blink, add clear search highlight keymap

--- dotfiles/nvim/lua/mappings.lua
+++ dotfiles/nvim/lua/mappings.lua
@@ -1,3 +1,4 @@
 -- Stay in indent mode
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("n", "<Leader>/", ":noh<cr>", { desc = "Clear highlight" })
--- dotfiles/nvim/lua/plugins/blink.lua
+++ dotfiles/nvim/lua/plugins/blink.lua
@@ -10,6 +10,10 @@ return {
 		completion = {
 			documentation = {
 				auto_show = true,
+				auto_show_delay_ms = 0,
+				window = {
+					border = "rounded",
+				},
 			},
 		},
 	},
